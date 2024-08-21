eb330e3464488a0569731d9a3d0ddbb715133470
Emanuel-Bissacotti <emanuelfbissacotti@gmail.com>
2024-08-21 12:58:04 -0300

    Adiciona um timer as ordenações sem threads concorrentes

--- Threads/main.py
+++ Threads/main.py
@@ -53,14 +53,16 @@ if __name__ == '__main__':
     end_time = time.time()
     print(f"Tempo de carregar os nomes do arquivo: {end_time - start_time} segundos")
 
-
     thread1 = threading.Thread(target=Ordering.sort_python, args=(names.copy(),))
     thread2 = threading.Thread(target=Ordering.quick_sort, args=(names.copy(),))
-    thread1.start()
     thread2.start()
+    thread1.start()
     thread1.join()
     thread2.join()
 
     print("Sem threads: ")
+    start_time = time.time()
     Ordering.sort_python(names=names.copy())
     Ordering.quick_sort(lista=names.copy())
+    end_time = time.time()
+    print(f"Tempo de ordenação sem threads: {end_time - start_time} segundos")
